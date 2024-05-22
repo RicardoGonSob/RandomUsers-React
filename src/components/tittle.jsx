404a7f15d9c3bbae5252b2578f0bccf3aba68d44
RicardoGonSob <21470005@campeche.tecnm.mx>
2024-05-22 10:27:18 -0600

    Version 3.0 Concluida

---
+++ src/components/tittle.jsx
@@ -0,0 +1,30 @@
+import './title.css';
+
+export default function Title({ newUser, deleteUsers }) {
+	const eventAddUser = () => {
+		newUser();
+	};
+	const eventDeleteUsers = () => {
+		deleteUsers();
+	};
+
+	return (
+		<>
+			<h1 className='title'>Random Users</h1>
+			<div className='buttons'>
+				<button
+					className='btn btn-add'
+					onClick={eventAddUser}
+				>
+					Añadir usuario
+				</button>
+				<button
+					className='btn btn-delete'
+					onClick={eventDeleteUsers}
+				>
+					Eliminar suspendidas
+				</button>
+			</div>
+		</>
+	);
+}
